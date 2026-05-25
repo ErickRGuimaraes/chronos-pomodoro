123177ddc3bab1addea4c9466495176fc797c838
Erick Rocha <erickguimaraes88526601@gmail.com>
2026-05-25 15:13:30 -0300

    feat: created count down component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Menu } from './components/Menu';
 import { Logo } from './components/Logo';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -10,8 +11,13 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
