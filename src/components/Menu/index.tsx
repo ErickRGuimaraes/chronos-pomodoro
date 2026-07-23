9d4fa7aeb6639cace34efd30eb38ccfa2bad5ec2
Erick Rocha <erickguimaraes88526601@gmail.com>
2026-07-23 14:15:47 -0300

    feat: change theme toghether icon

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,66 +1,82 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
-import styles from './styles.module.css';
+import type React from 'react';
 import { useEffect, useState } from 'react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+
+import styles from './styles.module.css';
 
 type Theme = 'dark' | 'light';
 
 export function Menu() {
-  const [theme, setTheme] = useState<Theme>('dark');
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = (localStorage.getItem('theme') as Theme) || 'dark';
+    return storedTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault();
+
     setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
-    // This is a simple way to toggle the theme, but in a real application, you might want to use a more robust solution, such as context or a state management library, to handle theme changes across the entire application.
-    // const root = document.documentElement;
-    // root.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
   }
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
     return () => {
       console.log('function clean up, because stack');
     };
   }, [theme]);
 
   return (
-    <>
-      <nav className={styles.menu}>
-        <a
-          className={styles.menuLink}
-          href='#'
-          arial-label='Go to home page'
-          title='Home'
-        >
-          <HouseIcon />
-        </a>
-        <a
-          className={styles.menuLink}
-          href='#'
-          arial-label='Go to history page'
-          title='History'
-        >
-          <HistoryIcon />
-        </a>
-        <a
-          className={styles.menuLink}
-          href='#'
-          arial-label='Go to settings page'
-          title='Settings'
-        >
-          <SettingsIcon />
-        </a>
-        <a
-          className={styles.menuLink}
-          href='#'
-          arial-label='Change theme mode'
-          title='Change theme mode'
-          onClick={handleThemeChange}
-        >
-          <SunIcon />
-        </a>
-      </nav>
-    </>
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Go to home page'
+        title='Home'
+      >
+        <HouseIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Go to history page'
+        title='History'
+      >
+        <HistoryIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Go to settings page'
+        title='Settings'
+      >
+        <SettingsIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Change theme mode'
+        title='Change theme mode'
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
   );
 }
