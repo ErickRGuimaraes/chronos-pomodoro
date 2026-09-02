375428ab2f25c009f34250a6b6197400c9209e9d
Erick Rocha <erickguimaraes88526601@gmail.com>
2026-09-02 16:59:13 -0300

    feat: add models taskModel and taskStateModel

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,14 @@
+import type { TaskModel } from './TaskModel';
+
+export type TaskStateModel = {
+  tasks: TaskModel[];
+  secondsRemaining: number;
+  formattedsecondsRemaining: string;
+  activeTaskId: TaskModel | null;
+  currentCycle: number;
+  config: {
+    workTime: number;
+    shortBreakTime: number;
+    longBreakTime: number;
+  };
+};
